2cbc85c7d171d599b9331d003c5eff8c16de4945
shykoh <declanshanahan@yahoo.com>
2025-05-23 02:02:52 +0100

    terratest

--- L5W6-main/test/sum.test.js
+++ L5W6-main/test/sum.test.js
@@ -2,10 +2,16 @@ function sum(a, b) {
   return a + b;
 }
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe('sum function', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 
-test('adds 5 + 10 to equal 15', () => {
-  expect(sum(5, 10)).toBe(15);
+  test('adds 5 + 10 to equal 15', () => {
+    expect(sum(5, 10)).toBe(15);
+  });
+
+  test('adds negative numbers correctly', () => {
+    expect(sum(-1, -5)).toBe(-6);
+  });
 });
